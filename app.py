3ffca1f62827e2a83c7e453e1e17e45b0b3c113b
Allen Stevens <allen.stevens@students.dominican.edu>
2025-11-11 17:58:53 -0800

    add user model and fix flask login

--- app.py
+++ app.py
@@ -18,6 +18,15 @@ login_manager.login_view = "login"
 login_manager.login_message_category = "info"
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    from models import User
+
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
+
 
 from routes import *
 
