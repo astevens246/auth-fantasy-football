bd001b0c1a5d8de9471addc63a53ddb7c15b6665
Allen Stevens <allen.stevens@students.dominican.edu>
2025-12-02 17:41:26 -0800

    add forms

--- forms.py
+++ forms.py
@@ -1,1 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+from models import User
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    
+class TeamForm(FlaskForm):
+    name = StringField('Team Name', validators=[DataRequired(), Length(min=3, max=100)])
+    submit = SubmitField('Create Team')
